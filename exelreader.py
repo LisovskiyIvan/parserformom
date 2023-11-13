bba265c13b7ece574360d01cb2ac03b88bf7aa62
Lisovskii Ivan <dnevanek@gmail.com>
2023-11-13 13:29:02 +0300

    working parser

--- exelreader.py
+++ exelreader.py
@@ -1,12 +1,48 @@
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
 
-book = pd.read_excel("parserformom/test2.xlsx", index_col=[0], skiprows=None)
+cookies = {
+    "__ddg1_": "h0qF7E0Xn9BghTzrYPHH",
+    "PHPSESSID": "M6S8dFIMQSLDiVCEyerwJuyQdP7HXxL9",
+    "_ym_uid": "1699804556468980057",
+    "_ym_d": "1699804556",
+    "_ym_isad": "1",
+}
 
-data = book.values
+headers = {
+    "authority": "spvb.ru",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "cache-control": "max-age=0",
+    # 'cookie': '__ddg1_=h0qF7E0Xn9BghTzrYPHH; PHPSESSID=M6S8dFIMQSLDiVCEyerwJuyQdP7HXxL9; _ym_uid=1699804556468980057; _ym_d=1699804556; _ym_isad=1',
+    "referer": "https://spvb.ru/rynki/fondovyy-rynok/",
+    "sec-ch-ua": '"Chromium";v="118", "Opera";v="104", "Not=A?Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0",
+}
+
+response = requests.get('https://spvb.ru/rynki/denezhnyy-rynok/uchastniki/', cookies=cookies, headers=headers).text
+
+# with open('result.html', 'w') as file:
+#     file.write(response)
+
+# with open("parserformom/result.html") as file:
+#     src = file.read()
+soup = bs(response, "lxml")
+table = soup.find_all("td")
 arr = []
-for z in data:
-    print(str(z)[0])
-    for i in z:
-        i = i
-        # print(i)
-print(book.values)
+for i in table:
+    arr.append(i.text)
+index = 2
+inns = []
+for z in range(int(len(arr)/6)):
+    inns.append(arr[index])
+    index = index + 6
+print(inns)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 import pandas as pd
+from exelreader import inns
 
 useragent = UserAgent()
 options = webdriver.ChromeOptions()
@@ -10,10 +11,11 @@ options.add_argument(f"user-agent={useragent.random}")
 driver = webdriver.Chrome(options=options)
 
 try:
-    book = pd.read_excel("parserformom/test2.xlsx", usecols="b")
-    data = book.values.tolist()
-    for z in data:
-        for i in z:
+    # book = pd.read_excel("parserformom/test2.xlsx", usecols="b")
+    # data = book.values.tolist()
+    # for z in data:
+    #     for i in z:
+    for i in inns:
             driver.get("https://egrul.nalog.ru/index.html")
             time.sleep(1)
             input_id = driver.find_element(By.ID, "query")
@@ -22,7 +24,7 @@ try:
             search_btn = driver.find_element(By.ID, "btnSearch").click()
             time.sleep(2)
             get_btn = driver.find_element(By.CLASS_NAME, "btn-excerpt").click()
-            time.sleep(1)
+            time.sleep(2)
 except Exception as ex:
     print(ex)
 
