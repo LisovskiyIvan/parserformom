6871c9ad97a2485c8f0562e41de440a855dcf24c
Lisovskii Ivan <dnevanek@gmail.com>
2023-11-21 23:14:01 +0300

    auto find initial path

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import pandas as pd
 from data import inns
 import os
 
+
 useragent = UserAgent()
 options = webdriver.ChromeOptions()
 options.add_argument(f"user-agent={useragent.random}")
 driver = webdriver.Chrome(options=options)
-path = r"C:\Users\johnyjabrony\Downloads"
-
+path = os.path.expanduser('~/Downloads')
 try:
     # book = pd.read_excel("parserformom/test3.xlsx", usecols="b")
     # data = book.values.tolist()
@@ -29,26 +29,17 @@ try:
             time.sleep(1)
             new_name = driver.find_element(By.CLASS_NAME, "op-excerpt").text
             time.sleep(1)
-            
-
-             # Путь к вашей папке
-            name_str = rf'C:\Users\johnyjabrony\Downloads\{new_name}.pdf'
-            # Получим список имен всего содержимого папки
-            # и превратим их в абсолютные пути
+        
+            name_str = rf'{path}\{new_name}.pdf'
             dir_list = [os.path.join(path, x) for x in os.listdir(path)]
 
             if dir_list:
-                # Создадим список из путей к файлам и дат их создания.
                 date_list = [[x, os.path.getctime(x)] for x in dir_list]
-
-                # Отсортируем список по дате создания в обратном порядке
                 sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)
-
-                # Выведем первый элемент списка. Он и будет самым последним по дате .strip(r"C:\Users\johnyjabrony\Downloads")
                 old_name = sort_date_list[0][0]
                 name_str = name_str.replace('"', '').replace('ПУБЛИЧНОЕ', 'П').replace('АКЦИОНЕРНОЕ', 'А').replace('ОБЩЕСТВО', 'О')
-            os.rename(old_name, name_str.replace('"', ''))
-
+            os.rename(old_name, name_str)
+    
 except Exception as ex:
     print(ex)
 
